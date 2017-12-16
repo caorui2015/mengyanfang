81cfef8e68d3c30d8e55ac710a031542ade21460
Itachi <627836535@qq.com>
2017-12-16 16:01:04 +0800

    1.0.1

--- pages/home/home.js
+++ pages/home/home.js
@@ -9,15 +9,18 @@ Page({
     list:[
       {
         "title": "合肥市蜀山区",
-        "image": imageAddress + "address.png"
+        "image": imageAddress + "address.png",
+        "id":"list1"
       },
       {
         "title": "10:00-20:00",
-        "image": imageAddress + "time.png"
+        "image": imageAddress + "time.png",
+        "id": "list2"
       },
       {
         "title": "13155463802",
-        "image": imageAddress + "operator.png"
+        "image": imageAddress + "operator.png",
+        "id": "list3"
       },
     ],
 
@@ -32,13 +35,24 @@ Page({
       imageAddress + "8.jpeg",
       imageAddress + "9.jpeg",
     ],
+    imageWidth: "87px",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var currentPage = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        var width = res.windowWidth;
+        var imageWidth = (width-44)/3;
+        currentPage.setData({
+          imageWidth: imageWidth + "px",
+        });
+        console.log(currentPage.data);
+      }
+    })
   },
 
   /**
@@ -88,5 +102,48 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  touchCell: function(e) {
+    var cellId = e.currentTarget.id;
+    if (cellId == "list1") {
+    } else if (cellId == "list2") {
+
+    }else {
+
+      this.callMeMaybe()
+    }
+  },
+
+  /**
+   * 打电话
+   */
+  callMeMaybe: function() {
+    wx.makePhoneCall({
+      phoneNumber: '13155463802',
+    })
+  },
+
+  /**
+   * 打开地点
+   */
+  showLocation: function() {
+
+  },
+
+
+  /**
+   * 显示图片
+   */
+  showImage: function (e) {
+    var currentPage = this;
+    var imageUrl = e.currentTarget.dataset.src;
+    wx.previewImage({
+      current: imageUrl,
+      urls: currentPage.data.imageList,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   }
 })
